5ec5cc4b37d7716c6c3323f0cdc401609cc2d29d
sungryul <airtrack03@naver.com>
2021-08-01 10:50:37 +0900

    BAEKJOON Greedy Silver Solving

--- Algorithm/BAEKJOON/Greedy/1931_회의실배정.py
+++ Algorithm/BAEKJOON/Greedy/1931_회의실배정.py
@@ -8,14 +8,11 @@ for i in range(N):
 
 data.sort(key=lambda time: (time[1], time[0]))
 
-start = 0
-end = 0
-ans = 0
+start, end, ans = 0, 0 , 0
 
 for d in data:
     if d[0] >= end:
-        start = d[0]
-        end = d[1]
+        start, end = d[0], d[1]
         ans += 1
 
 print(ans)
