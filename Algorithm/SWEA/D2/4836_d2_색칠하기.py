0e0c9f9ae33e5ffdad6e3f500aa6d358b9e7b44c
sungryul <airtrack03@naver.com>
2021-08-13 18:13:12 +0900

    4836_d2 Modified

--- Algorithm/SWEA/D2/4836_d2_색칠하기.py
+++ Algorithm/SWEA/D2/4836_d2_색칠하기.py
@@ -1,3 +1,7 @@
+import sys
+
+sys.stdin = open("input.txt")
+
 T = int(input())
 
 for tc in range(1, T + 1):
@@ -9,8 +13,11 @@ for tc in range(1, T + 1):
         x_start, y_start, x_end, y_end, color = map(int, input().split())
         for y in range(y_start, y_end + 1):
             for x in range(x_start, x_end + 1):
-                area[y][x] += color
-                if area[y][x] >= 3 and (x, y) not in purple:  # 이미 보라색인데 또 덧칠했을 때 중복해서 더해지는 것 방지
+                # 같은 색이 중복해서 칠해지지 않도록 조건 필요
+                if area[y][x] == color or area[y][x] == 3:  # 칠하려던 색으로 같은 색이 칠해져 있거나 이미 보라색인 경우
+                    continue
+                area[y][x] += color  # 다른 색이 칠해져있거나 아무 것도 칠해져있지 않은 경우
+                if area[y][x] == 3:  # 위 조건 추가로 더이상 보라색 위에 덧칠해지는 경우 없으므로 not in 불필요
                     purple.append((x, y))
     ans = len(purple)
 
